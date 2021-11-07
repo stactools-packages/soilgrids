fe8e435eac5b9cbd8e69769e2c37b2a9a369a1a2
Tyler Battle <tyler@battle.ca>
2021-11-07 14:11:05 -0800

    Cleanup constants

--- src/stactools/soilgrids/constants.py
+++ src/stactools/soilgrids/constants.py
@@ -1,10 +1,12 @@
 # flake8: noqa
 
+from datetime import date
+
 from pystac import Link, Provider, ProviderRole
 
-SOILGRIDS_ID = "SoilGrids"
-SOILGRIDS_EPSG = 152160
-SOILGRIDS_CRS_WKT = """PROJCS["Homolosine",
+COLLECTION_ID = "SoilGrids"
+EPSG = 152160
+CRS_WKT = """PROJCS["Homolosine",
     GEOGCS["WGS 84",
         DATUM["WGS_1984",
             SPHEROID["WGS 84",6378137,298.257223563,
@@ -17,7 +19,7 @@ SOILGRIDS_CRS_WKT = """PROJCS["Homolosine",
         AUTHORITY["EPSG","4326"]],
     PROJECTION["Interrupted_Goode_Homolosine"],
     UNIT["Meter",1]]"""
-SOILGRIDS_TITLE = "ISRIC SoilGrids Global Soil Property Maps"
+TITLE = "ISRIC SoilGrids Global Soil Property Maps"
 LICENSE = "CC-BY-4.0"
 LICENSE_LINK = Link(
     rel="license",
@@ -28,9 +30,9 @@ LICENSE_LINK = Link(
 DATASET_URL = "https://files.isric.org/soilgrids/latest/data/"
 DATASET_ACCESS_URL = f"/vsicurl?max_retry=3&retry_delay=1&list_dir=no&url={DATASET_URL}"
 
-SOILGRIDS_DESCRIPTION = """SoilGridsTM (hereafter SoilGrids) is a system for global digital soil mapping that makes use of global soil profile information and covariate data to model the spatial distribution of soil properties across the globe. SoilGrids is a collections of soil property maps for the world produced using machine learning at 250 m resolution."""
+DESCRIPTION = """SoilGridsTM (hereafter SoilGrids) is a system for global digital soil mapping that makes use of global soil profile information and covariate data to model the spatial distribution of soil properties across the globe. SoilGrids is a collections of soil property maps for the world produced using machine learning at 250 m resolution."""
 
-SOILGRIDS_PROVIDER = Provider(
+PROVIDER = Provider(
     name="ISRIC — World Soil Information",
     roles=[
         ProviderRole.HOST,
@@ -40,42 +42,39 @@ SOILGRIDS_PROVIDER = Provider(
     url=
     "https://www.isric.org/explore/soilgrids")
 
-SOILGRIDS_BOUNDING_BOX = [96.00, -44.00, 168.00, -9.00]
-SOILGRIDS_START_YEAR = "2021"
-SOILGRIDS_END_YEAR = "2021"
+BOUNDING_BOX = [96.00, -44.00, 168.00, -9.00]
+RELEASE_DATE = date(2020, 5, 1)
 
 DOI = "10.5194/soil-7-217-2021"
 CITATION = "Poggio, L., de Sousa, L. M., Batjes, N. H., Heuvelink, G. B. M., Kempen, B., Ribeiro, E., and Rossiter, D.: SoilGrids 2.0: producing soil information for the globe with quantified spatial uncertainty, SOIL, 7, 217–240, 2021."
 
-#pH, soil organic carbon content, bulk density, coarse fragments content, sand content, silt content, clay content, cation exchange capacity (CEC), total nitrogen as well as soil organic carbon density and soil organic carbon stock
-
 SOIL_PROPERTIES = {
     "bdod": "Bulk density of the fine earth fraction (cg/cm³)",
-    # "cec": "Cation Exchange Capacity of the soil (mmol(c)/kg)",
-    # "cfvo": "",
-    # "clay": "",
-    # "nitrogen": "",
-    # "ocd": "",
-    # "ocs": "",  # only 0-30cm
-    # "phh2o": "",
-    # "sand": "",
-    # "silt": "",
-    # "soc": "",
+    "cec": "Cation Exchange Capacity of the soil (mmol(c)/kg)",
+    "cfvo": "Volumetric fraction of coarse fragments (> 2 mm) (cm3/dm3 (vol‰))",
+    "clay": "Proportion of clay particles (< 0.002 mm) in the fine earth fraction (g/kg)",
+    "nitrogen": "Total nitrogen (cg/kg)",
+    "ocd": "Organic carbon density (hg/m³)",
+    "ocs": "Organic carbon stocks (t/ha)",  # only 0-30cm
+    "phh2o": "Soil pH (pHx10)",
+    "sand": "Proportion of sand particles (> 0.05 mm) in the fine earth fraction (g/kg)",
+    "silt": "Proportion of silt particles (≥ 0.002 mm and ≤ 0.05 mm) in the fine earth fraction (g/kg)",
+    "soc": "Soil organic carbon content in the fine earth fraction (dg/kg)",
 }
 DEPTHS = {
     "0-5cm": "Zero to 5cm Depth",
-    # "5-15cm": "5cm to 15cm Depth",
-    # "15-30cm": "15cm to 30cm Depth",
-    # "30-60cm": "30cm to 60cm Depth",
-    # "60-100cm": "60cm to 100cm Depth",
-    # "100-200cm": "100cm to 200cm Depth",
+    "5-15cm": "5cm to 15cm Depth",
+    "15-30cm": "15cm to 30cm Depth",
+    "30-60cm": "30cm to 60cm Depth",
+    "60-100cm": "60cm to 100cm Depth",
+    "100-200cm": "100cm to 200cm Depth",
 }
 PROBS = {
-    "Q0.05": "",
-    # "Q0.5": "",
-    # "Q0.95": "",
-    # "mean": "",
-    # "uncertainty": "",
+    "Q0.05": "5% quantile",
+    "Q0.5": "median of the distribution",
+    "Q0.95": "95% quantile",
+    "mean": "mean of the distribution",
+    "uncertainty": "10x(Q0.95-Q0.05)/Q0.50",
 }
 
 TILING_PIXEL_SIZE = (10000, 10000)
