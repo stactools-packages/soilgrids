299b1adae3bf6239151bf9c6a61ed468acb16890
Tyler Battle <tyler@battle.ca>
2021-11-24 17:33:31 -0800

    Create stub tests. These only verify that the commands run successfully, not that the results are valid.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -5,17 +5,16 @@ import pystac
 from stactools.testing import CliTestCase
 
 from stactools.soilgrids.commands import create_soilgrids_command
+from tests import test_data
 
 
 class CommandsTest(CliTestCase):
     def create_subcommand_functions(self):
         return [create_soilgrids_command]
 
+    # TODO: Stub test. It only ensures completion of the command
     def test_create_collection(self):
         with TemporaryDirectory() as tmp_dir:
-            # Run your custom create-collection command and validate
-
-            # Example:
             destination = os.path.join(tmp_dir, "collection.json")
 
             result = self.run_command(
@@ -29,32 +28,30 @@ class CommandsTest(CliTestCase):
             self.assertEqual(len(jsons), 1)
 
             collection = pystac.read_file(destination)
-            self.assertEqual(collection.id, "my-collection-id")
-            # self.assertEqual(item.other_attr...
+            self.assertEqual(collection.id, "soilgrids250m")
 
             collection.validate()
 
+    # TODO: Stub test. It only ensures completion of the command
     def test_create_item(self):
         with TemporaryDirectory() as tmp_dir:
-            # Run your custom create-item command and validate
+            test_path = test_data.get_path("data-files/cogs")
+            for asset_dir in filter(os.path.isdir, os.listdir(test_path)):
+                destination = os.path.join(tmp_dir, "collection.json")
+                result = self.run_command([
+                    "soilgrids",
+                    "create-item",
+                    os.path.join(test_path, asset_dir),
+                    destination,
+                ])
+                self.assertEqual(result.exit_code,
+                                 0,
+                                 msg="\n{}".format(result.output))
 
-            # Example:
-            destination = os.path.join(tmp_dir, "collection.json")
-            result = self.run_command([
-                "soilgrids",
-                "create-item",
-                "/path/to/asset.tif",
-                destination,
-            ])
-            self.assertEqual(result.exit_code,
-                             0,
-                             msg="\n{}".format(result.output))
+                jsons = [p for p in os.listdir(tmp_dir) if p.endswith(".json")]
+                self.assertEqual(len(jsons), 1)
 
-            jsons = [p for p in os.listdir(tmp_dir) if p.endswith(".json")]
-            self.assertEqual(len(jsons), 1)
+                item = pystac.read_file(destination)
+                self.assertEqual(item.id, "soilgrids250m")
 
-            item = pystac.read_file(destination)
-            self.assertEqual(item.id, "my-item-id")
-            # self.assertEqual(item.other_attr...
-
-            item.validate()
+                item.validate()
